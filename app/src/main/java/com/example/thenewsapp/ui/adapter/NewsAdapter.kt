499c5e217f54dc49de10b869c1afd8c5dd4d03f5
VUQUOCCHINH <chinhvu2002@gmail.com>
2024-04-18 13:40:19 +0700

    text to audio

--- app/src/main/java/com/example/thenewsapp/ui/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/thenewsapp/ui/adapter/NewsAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.thenewsapp.ui.adapter
 
-class NewsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.thenewsapp.R
+import com.example.thenewsapp.databinding.ItemNewsBinding
+import com.example.thenewsapp.model.News
+
+class NewsAdapter(private val context: Context, private var news: List<News>, private val listener: (News, Int) -> Unit): RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
+    class NewsHolder(val binding: ItemNewsBinding) : RecyclerView.ViewHolder(binding.root)
+
+    fun update(news: List<News>) {
+        this.news = news
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
+        val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return news.size
+    }
+
+    override fun onBindViewHolder(holder: NewsHolder, position: Int) {
+        val new = news[position]
+        holder.binding.articleDateTime.text = new.pubDate
+        holder.binding.articleDescription.text = new.description
+        holder.binding.articleTitle.text = new.title
+        Glide.with(context).load(new.imageURL)
+            .placeholder(R.drawable.ic_launcher_background).error(R.drawable.ic_launcher_background).into(holder.binding.articleImage)
+        holder.binding.container.setOnClickListener {
+            listener(new, position)
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/thenewsapp/ui/flagments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/flagments/HeadlinesFragment.kt
@@ -1,26 +1,42 @@
 package com.example.thenewsapp.ui.flagments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.thenewsapp.R
+import com.example.thenewsapp.api.ApiService
+import com.example.thenewsapp.api.RetrofitClient
+import com.example.thenewsapp.databinding.FragmentHeadlinesBinding
+import com.example.thenewsapp.model.News
+import com.example.thenewsapp.model.TopNewResponse
+import com.example.thenewsapp.ui.activities.NewsDetailAct
+import com.example.thenewsapp.ui.adapter.NewsAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HeadlinesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HeadlinesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private val listNew = mutableListOf<News>()
+    private lateinit var newAdapter: NewsAdapter
+    private lateinit var binding: FragmentHeadlinesBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,21 +49,52 @@ class HeadlinesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false)
+    ): View {
+        binding = FragmentHeadlinesBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        initData()
+    }
+
+    private fun initData() {
+        val apiService = RetrofitClient.getInstance().create(ApiService::class.java)
+        apiService.fetchData("vi", "pub_42045ddca4905cd301e552ee7e03f781fa608").enqueue(object :
+            Callback<TopNewResponse> {
+            override fun onResponse(
+                call: Call<TopNewResponse>,
+                response: Response<TopNewResponse>
+            ) {
+                binding.paginationProgressBar.isVisible = false
+                if(response.isSuccessful) {
+                    newAdapter.update(response.body()!!.results)
+                }
+            }
+
+            override fun onFailure(call: Call<TopNewResponse>, t: Throwable) {
+                binding.paginationProgressBar.isVisible = false
+                Toast.makeText(requireContext(), t.message, Toast.LENGTH_LONG).show()
+            }
+        })
+
+    }
+
+    private fun initViews() {
+        binding.paginationProgressBar.isVisible = true
+        newAdapter = NewsAdapter(requireContext(), listNew) { new, _ ->
+            val intent = Intent(requireContext(), NewsDetailAct::class.java)
+            intent.putExtra("data", new)
+            startActivity(intent)
+        }
+        binding.recyclerHeadlines.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerHeadlines.adapter = newAdapter
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HeadlinesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HeadlinesFragment().apply {
